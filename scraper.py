e3fc381707f740df7d0dd16ccc0a75fe15a2e762
Jason Lee <leej48@uci.edu>
2026-02-07 15:32:31 -0800

    q1 and q1 of report code

--- scraper.py
+++ scraper.py
@@ -5,6 +5,43 @@ from urllib.parse import urldefrag
 from bs4 import BeautifulSoup
 from utils.response import Response
 
+STOPWORDS = {
+    "a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at",
+    "be","because","been","before","being","below","between","both","but","by","can't","cannot","could",
+    "couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for",
+    "from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's",
+    "her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm",
+    "i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't",
+    "my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours",
+    "ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't",
+    "so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there",
+    "there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too",
+    "under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't",
+    "what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's",
+    "with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself",
+    "yourselves"
+}
+
+unique_urls = set()
+longest_page = ("", 0) # (url, word_count)
+
+def tokenize_text(text: str):
+    tokens = []
+    token = ""
+
+    for char in text.lower():
+        if char.isalnum() and char.isascii():
+            token += char
+        else:
+            if token:
+                tokens.append(token)
+                token = ""
+
+    if token:
+        tokens.append(token)
+
+    return tokens
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -19,7 +56,8 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    hyperlinks = list()
+    global longest_page
+    hyperlinks = set()
 
     if 600 <= resp.status <= 608:
         print(f"Error: {resp.error}")
@@ -30,20 +68,37 @@ def extract_next_links(url, resp):
         return hyperlinks
     
     soup = BeautifulSoup(resp.raw_response.content, "html.parser")
+    
+    for tag in soup(["script", "style", "noscript"]):
+        tag.decompose()
+    
     links = soup.find_all("a", href=True)
+
+        
+    page_url, _ = urldefrag(resp.url)
+    unique_urls.add(page_url)
+
+    text = soup.get_text(separator=" ")
+    tokens = tokenize_text(text)
+    filtered = list()
+
+    for word in tokens:
+        if word not in STOPWORDS:
+            filtered.append(word)
+
+    word_count = len(filtered)
+
+    if word_count > longest_page[1]:
+        longest_page = (page_url, word_count)
 
     for alink in links:
         link = urljoin(resp.url, alink["href"])
         clean_url, fragment = urldefrag(link)
-        hyperlinks.append(clean_url)
-        if is_valid(clean_url):
-            print('this is a link', clean_url)
+        hyperlinks.add(clean_url)
+        # if is_valid(clean_url):
+        #     print('this is a link', clean_url)
 
-    # for i in range(5):
-    #     print(hyperlinks[i])
-
-    
-    return hyperlinks
+    return list(hyperlinks)
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
